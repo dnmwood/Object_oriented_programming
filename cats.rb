8beb768c3d6767ca8e1cce65dd911ad14d0bd244
David Wood <davidnmwood@gmail.com>
2016-10-20 13:42:46 -0500

    create Fred and Billy

--- cats.rb
+++ cats.rb
@@ -1,5 +1,23 @@
 class Cat
 
   def initialize(name, preferred_food, meal_time)
+  @name           = name
+  @preferred_food = preferred_food
+  @meal_time      = meal_time
+  end
 
+
+  def eats
+    @meal_time
+  end
+
+  def name
+    "#{ @name }"
+  end
 end
+
+fred = Cat.new("Fred", "tuna", 16)
+billy = Cat.new("Billy", "chicken", 12)
+
+puts fred.eats
+puts billy.eats
